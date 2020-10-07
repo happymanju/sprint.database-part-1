8b7ebdcfccb132a12c4436fed3e1c29814d16ba0
Jack Fowler <OnigiriJack@users.noreply.github.com>
2020-10-07 17:48:34 +0900

    find oldest person in each town
    Co-authored-by: OnigiriJack <onigirijack@users.noreply.github.com>
    Co-authored-by: happymanju <happymanju@users.noreply.github.com>

--- scripts/selecting/oldestInTown.sql
+++ scripts/selecting/oldestInTown.sql
@@ -1,1 +1,4 @@
 -- Your SQL here
+SELECT town_of_origin, first_name, min(date_of_birth) FROM students GROUP BY town_of_origin, first_name;
+
+SELECT town_of_origin, min(date_of_birth) FROM students GROUP BY town_of_origin;
